4ce7ac484e74588313269c38beb5b48e08749ed5
aljhun1 <kairoseditor12@gmail.com>
2024-09-23 14:42:46 +0800

    SORRY SIR NA LATE ILOVEYOUU MWAAAAAAA

---
+++ src/gilbuena/Sale.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gilbuena;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author SCC
+ */
+public class Sale {
+      public void getSales(){
+        Scanner input = new Scanner(System.in);
+        Sales sls = new Sales();
+        
+        System.out.println("Running Sales Program:\n");
+        System.out.print("Enter Customer name: ");
+        String CNAME = input.nextLine();
+        System.out.print("Enter Product Name: ");
+        String PNAME = input.nextLine();
+        System.out.print("Enter Quantity: ");
+        int QTY = input.nextInt();
+        System.out.print("Enter Price: ");
+        int PRICE = input.nextInt();
+        System.out.print("Enter Cash: ");
+        int CASH = input.nextInt();
+        
+        sls.addSales(CNAME, PNAME, QTY, PRICE, CASH);
+        sls.viewSales();
+    }
+
+}
